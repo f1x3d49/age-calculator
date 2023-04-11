ee68ee2733a4512ed544ee93ca9fa6a507bc4c91
f1x3d49 <davidnicoara28@gmail.com>
2023-04-12 00:55:07 +0300

    did the form validation

--- frontend/src/components/FormComponent.js
+++ frontend/src/components/FormComponent.js
@@ -1,16 +1,46 @@
 import React, { useState, createContext } from "react";
 import { ReactComponent as Arrow } from "../assets/images/icon-arrow.svg";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 export const BDayContext = createContext();
 
 const FormComponent = () => {
-  const [day, setDay] = useState("");
-  const [month, setMonth] = useState("");
-  const [year, setYear] = useState("");
   const [age, setAge] = useState("");
-  const [dayerror, setDError] = useState("");
-  const [montherror, setMError] = useState("");
-  const [yearerror, setYError] = useState("");
+
+  //Formik Logics
+  const formik = useFormik({
+    initialValues: {
+      day: "",
+      month: "",
+      year: "",
+    },
+
+    //Form Validation
+    validationSchema: Yup.object({
+      day: Yup.number()
+        .required("This field is required")
+        .min(1, "Must be a valid day")
+        .max(31, "Must be a valid day"),
+      month: Yup.number()
+        .required("This field is required")
+        .min(1, "Must be a valid month")
+        .max(12, "Must be a valid month"),
+      year: Yup.number()
+        .required("This field is required")
+        .min(1900, "Must be a more recent year")
+        .max(2023, "Must be in the past"),
+    }),
+
+    //Submit Form
+    onSubmit: (values) => {
+      setAge(
+        calculateAge(
+          `${formik.values.month} ${formik.values.day} ${formik.values.year}`
+        )
+      );
+    },
+  });
 
   const generateArray = (start, end) => {
     let arr = [];
@@ -18,23 +48,6 @@ const FormComponent = () => {
       arr.push(start);
     }
     return arr;
-  };
-
-  // validate function
-  const validate = () => {
-    const errors = {};
-    const months = generateArray(1, 12);
-    const days = generateArray(1, 31);
-    const years = generateArray(1900, new Date().getFullYear());
-
-    if (day === "") errors.day = "This field is required";
-    if (!days.includes(day)) errors.day = "Must be a valid day";
-
-    if (month === "") errors.month = "This field is required";
-    if (!months.includes(month)) errors.month = "Must be a valid month";
-
-    if (year === "") errors.year = "This field is required";
-    if (!years.includes(year)) errors.year = "Must be a valid year";
   };
 
   // calculating age function
@@ -51,54 +64,91 @@ const FormComponent = () => {
     return `${years} years ${months} months ${days} days`;
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setAge(calculateAge(`${month} ${day} ${year}`));
-  };
-
   return (
-    <form onSubmit={handleSubmit} className="w-full">
+    <form onSubmit={formik.handleSubmit} className="w-full">
       <div className="flex gap-6">
         <div className="flex flex-col">
-          <label className="text-secondsgrey tracking-widest text-xs font-bold mb-2">
+          <label
+            htmlFor="day"
+            className={`text-secondsgrey tracking-widest text-xs font-bold mb-2 ${
+              formik.touched.day && formik.errors.day ? "text-primaryred" : ""
+            }`}
+          >
             DAY
           </label>
           <input
             type="text"
-            required
-            value={day}
-            onChange={(e) => setDay(e.target.value)}
+            name="day"
+            value={formik.values.day}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
             placeholder="DD"
-            className="appearance-none pl-5 w-32 h-14 border border-secondlgrey rounded-md placeholder:text-2xl placeholder:font-bold placeholder:text-secondsgrey"
+            className={`appearance-none pl-5 w-32 h-14 border border-secondlgrey focus:outline-none rounded-md placeholder:text-2xl placeholder:font-bold placeholder:text-secondsgrey ${
+              formik.touched.day && formik.errors.day ? "border-primaryred" : ""
+            }`}
           />
+          <div className="text-xs font-normal italic text-primaryred mt-1">
+            {formik.touched.day && formik.errors.day ? formik.errors.day : ""}
+          </div>
         </div>
-
         <div className="flex flex-col">
-          <label className="text-secondsgrey tracking-widest text-xs font-bold mb-2">
+          <label
+            htmlFor="month"
+            className={`text-secondsgrey tracking-widest text-xs font-bold mb-2 ${
+              formik.touched.month && formik.errors.month
+                ? "text-primaryred"
+                : ""
+            }`}
+          >
             MONTH
           </label>
           <input
             type="text"
-            required
-            value={month}
-            onChange={(e) => setMonth(e.target.value)}
+            name="month"
+            value={formik.values.month}
+            onChange={formik.handleChange}
             placeholder="MM"
-            className=" appearance-none pl-5 w-32 h-14 border border-secondlgrey rounded-md placeholder:text-2xl placeholder:font-bold placeholder:text-secondsgrey"
+            className={`appearance-none pl-5 w-32 h-14 border border-secondlgrey focus:outline-none rounded-md placeholder:text-2xl placeholder:font-bold placeholder:text-secondsgrey ${
+              formik.touched.month && formik.errors.month
+                ? "border-primaryred"
+                : ""
+            }`}
           />
+          <div className="text-xs font-normal italic text-primaryred mt-1">
+            {formik.touched.month && formik.errors.month
+              ? formik.errors.month
+              : ""}
+          </div>
         </div>
 
         <div className="flex flex-col">
-          <label className="text-secondsgrey tracking-widest text-xs font-bold mb-2">
+          <label
+            htmlFor="year"
+            className={`text-secondsgrey tracking-widest text-xs font-bold mb-2 ${
+              formik.touched.month && formik.errors.month
+                ? "text-primaryred"
+                : ""
+            }`}
+          >
             YEAR
           </label>
           <input
             type="text"
-            required
-            value={year}
-            onChange={(e) => setYear(e.target.value)}
+            name="year"
+            value={formik.values.year}
+            onChange={formik.handleChange}
             placeholder="YYYY"
-            className="block appearance-none pl-5 w-32 h-14 border border-secondlgrey rounded-md placeholder:text-2xl placeholder:font-bold placeholder:text-secondsgrey"
+            className={`block appearance-none pl-5 w-32 h-14 border border-secondlgrey focus:outline-none rounded-md placeholder:text-2xl placeholder:font-bold placeholder:text-secondsgrey ${
+              formik.touched.year && formik.errors.year
+                ? "border-primaryred"
+                : ""
+            }`}
           />
+          <div className="text-xs font-normal italic text-primaryred mt-1">
+            {formik.touched.year && formik.errors.year
+              ? formik.errors.year
+              : ""}
+          </div>
         </div>
       </div>
       <div className="flex">
